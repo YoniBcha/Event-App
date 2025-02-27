dd016669eef18be7fef3ebbfa4545123e33252ea
bekizod <bekizodcancer@gmail.com>
2025-02-27 21:29:58 +0300

    updating the grid view for choosePackage

--- components/bookingstep/ChooseDesigns.tsx
+++ components/bookingstep/ChooseDesigns.tsx
@@ -102,46 +102,46 @@ function ChooseDesigns({ id, onNext, onBackClick }: ChooseDesignsProps) {
   // Check if there are no designs
   if (!isLoading && designs.length === 0) {
     return (
-        <motion.div
-                    className="flex flex-col gap-10 h-full justify-center items-center"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                  >
-              <div className="text-primary font-bold text-xl md:text-3xl text-center">
-                {translations.booking.noDesign}
-              </div>
-      
-                   {/* Back Button */}
-              <motion.button
-                onClick={onBackClick}
-                className="back-btn flex items-center hover:bg-secondary p-2 rounded-lg border border-primary text-primary cursor-pointer"
-                variants={{
-                  hover: {
-                    scale: 1.05,
-                    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
-      
-                    transition: { duration: 0.2, ease: "easeInOut" },
-                  },
-                  tap: {
-                    scale: 0.95,
-                    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
-                    transition: { duration: 0.1, ease: "easeInOut" },
-                  },
-                }}
-                whileHover="hover"
-                whileTap="tap"
-              >
-                <span className="mr-2">
-                  {currentLocale === "ar" ? (
-                    <FaChevronRight /> // Right arrow for Arabic
-                  ) : (
-                    <FaChevronLeft /> // Left arrow for English
-                  )}
-                </span>
-                <span>{translations.booking.backBtn}</span>
-              </motion.button>
-             </motion.div>
+      <motion.div
+        className="flex flex-col gap-10 h-full justify-center items-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <div className="text-primary font-bold text-xl md:text-3xl text-center">
+          {translations.booking.noDesign}
+        </div>
+
+        {/* Back Button */}
+        <motion.button
+          onClick={onBackClick}
+          className="back-btn flex items-center hover:bg-secondary p-2 rounded-lg border border-primary text-primary cursor-pointer"
+          variants={{
+            hover: {
+              scale: 1.05,
+              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+
+              transition: { duration: 0.2, ease: "easeInOut" },
+            },
+            tap: {
+              scale: 0.95,
+              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
+              transition: { duration: 0.1, ease: "easeInOut" },
+            },
+          }}
+          whileHover="hover"
+          whileTap="tap"
+        >
+          <span className="mr-2">
+            {currentLocale === "ar" ? (
+              <FaChevronRight /> // Right arrow for Arabic
+            ) : (
+              <FaChevronLeft /> // Left arrow for English
+            )}
+          </span>
+          <span>{translations.booking.backBtn}</span>
+        </motion.button>
+      </motion.div>
     );
   }
 
@@ -164,41 +164,43 @@ function ChooseDesigns({ id, onNext, onBackClick }: ChooseDesignsProps) {
         // Grid View
         <>
           <div className="flex sm:hidden flex-col gap-4 w-full px-4">
-            {designs.map((design: Design, index: number) => (
-              <motion.div
-                key={design._id || index}
-                className={`flex flex-col justify-center hover:bg-secondary items-center bg-gray-100 rounded-lg overflow-hidden cursor-pointer p-2 transition-all duration-300 ${
-                  selectedDesignId === design._id
-                    ? "border-2 border-primary scale-105"
-                    : "border border-gray-300"
-                }`}
-                onClick={() => handleCardClick(design._id)}
-                variants={slideVariants}
-                initial="hidden"
-                animate="visible"
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-              >
-                <div
-                  className="relative w-full h-48"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    handleImageClick(design.image);
-                  }}
+            <div className="grid grid-cols-1 gap-4 [@media(min-width:361px)]:grid-cols-2 [@media(min-width:639px)]:grid-cols-2">
+              {designs.map((design: Design, index: number) => (
+                <motion.div
+                  key={design._id || index}
+                  className={`flex flex-col justify-center hover:bg-secondary items-center bg-gray-100 rounded-lg overflow-hidden cursor-pointer p-2 transition-all duration-300 ${
+                    selectedDesignId === design._id
+                      ? "border-2 border-primary scale-105"
+                      : "border border-gray-300"
+                  }`}
+                  onClick={() => handleCardClick(design._id)}
+                  variants={slideVariants}
+                  initial="hidden"
+                  animate="visible"
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
                 >
-                  <Image
-                    src={design.image}
-                    alt={design.eventDesign}
-                    layout="fill"
-                    objectFit="cover"
-                    className="rounded-t-lg"
-                  />
-                </div>
-                <p className="mt-2 text-sm text-tertiary font-medium text-center">
-                  {design.eventDesign}
-                </p>
-              </motion.div>
-            ))}
+                  <div
+                    className="relative w-full h-48"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleImageClick(design.image);
+                    }}
+                  >
+                    <Image
+                      src={design.image}
+                      alt={design.eventDesign}
+                      layout="fill"
+                      objectFit="cover"
+                      className="rounded-t-lg"
+                    />
+                  </div>
+                  <p className="mt-2 text-sm text-tertiary font-medium text-center">
+                    {design.eventDesign}
+                  </p>
+                </motion.div>
+              ))}
+            </div>
           </div>
 
           <div className="w-[90%] px-4 sm:block hidden relative">
