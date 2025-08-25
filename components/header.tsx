af75ecdd273a165772d9ed8fe256dce651234695
bekizod <bekizodcancer@gmail.com>
2025-08-25 21:38:31 +0300

    profile image updated

--- components/header.tsx
+++ components/header.tsx
@@ -268,7 +268,7 @@ function Header() {
             </div>
             <div
               className="h-10 w-10 rounded-full bg-cover bg-center cursor-pointer circle-button"
-              style={{ backgroundImage: "url('/pp2.jpg')" }}
+              style={{ backgroundImage: "url('/profile-icon-02.png')" }}
               onClick={toggleAvatarDropdown}
             >
               <AnimatePresence>
